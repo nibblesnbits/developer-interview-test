011450676fc6eaeaa03238d0434c14f54ef08b7b
Stephen Collins <sgcollins@outlook.com>
2023-04-28 20:57:45 -0400

    super pretentious version

--- Smartwyre.DeveloperTest/Services/RebateService.cs
+++ Smartwyre.DeveloperTest/Services/RebateService.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Monads;
+﻿using Monads;
 using Smartwyre.DeveloperTest.Data;
 using Smartwyre.DeveloperTest.Types;
 
@@ -19,37 +17,15 @@ public class RebateService : IRebateService
 
     public Maybe<decimal> Calculate(CalculateRebateRequest request)
     {
-        var rebate = _rebateDataStore.GetRebate(request.RebateIdentifier);
-
-        if (rebate is null)
-        {
-            return Maybe<decimal>.Empty;
-        }
-
-        var product = _productDataStore.GetProduct(request.ProductIdentifier);
-
-        if (product is null)
-        {
-            return Maybe<decimal>.Empty;
-        }
-
-        if (product.SupportedIncentives.HasFlag(rebate.Incentive))
-        {
-            return Maybe<decimal>.Empty;
-        }
-
-        var rebateAmount = rebate switch
-        {
-            { Incentive: var i, Amount: var a } when i == IncentiveType.FixedCashAmount && rebate.Amount != 0 => rebate.Amount,
-            { Incentive: var i, Percentage: var p } when i == IncentiveType.FixedRateRebate && (p != 0 || product.Price != 0 || request.Volume != 0) => product.Price * p * request.Volume,
-            { Incentive: var i, Amount: var a } when i == IncentiveType.AmountPerUom && (a != 0 || request.Volume != 0) => a * request.Volume,
-            _ => 0
-        };
-
-        if (rebateAmount == 0)
-        {
-            return Maybe<decimal>.Empty;
-        }
+        var rebateAmount = _rebateDataStore.GetRebate(request.RebateIdentifier)
+            .SelectMany((rebate) => _productDataStore.GetProduct(request.ProductIdentifier),
+                (r, p) => p.SupportedIncentives.HasFlag(r.Incentive) ? 0 : r switch
+                {
+                    { Incentive: var i, Amount: var a } when i == IncentiveType.FixedCashAmount && r.Amount != 0 => r.Amount,
+                    { Incentive: var i, Percentage: var pc } when i == IncentiveType.FixedRateRebate && (pc != 0 || p.Price != 0 || request.Volume != 0) => p.Price * pc * request.Volume,
+                    { Incentive: var i, Amount: var a } when i == IncentiveType.AmountPerUom && (a != 0 || request.Volume != 0) => a * request.Volume,
+                    _ => 0
+                });
 
         return rebateAmount;
     }
