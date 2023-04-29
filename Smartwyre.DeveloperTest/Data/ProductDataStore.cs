b528b9f05fea313a4e97f7edf295288bfebd65af
Stephen Collins <sgcollins@outlook.com>
2023-04-28 20:55:39 -0400

    forgot a Maybe

--- Smartwyre.DeveloperTest/Data/ProductDataStore.cs
+++ Smartwyre.DeveloperTest/Data/ProductDataStore.cs
@@ -1,10 +1,11 @@
-﻿using Smartwyre.DeveloperTest.Types;
+﻿using Monads;
+using Smartwyre.DeveloperTest.Types;
 
 namespace Smartwyre.DeveloperTest.Data;
 
 public class ProductDataStore
 {
-    public Product GetProduct(string productIdentifier)
+    public Maybe<Product> GetProduct(string productIdentifier)
     {
         // Access database to retrieve account, code removed for brevity 
         return new Product();
